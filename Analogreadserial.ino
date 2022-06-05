d1b54f916d2a232907c1262c88ad8c7895cac91e
Surafelll <surafelwondimagegn01@gmail.com>
2022-06-05 09:03:54 +0300

    input analog,print out value you read,delay between stability

--- Analogreadserial.ino
+++ Analogreadserial.ino
@@ -3,5 +3,10 @@ Arduino Board
 10k ohm Potentiometer*/
 void void setup()
 {
-    Serial.begin(9600);serial port
+    Serial.begin(9600); //serial port
+}
+void loop(){
+    int sensorvalue = analogRead(A10);
+    serial.printl(sensorvalue);
+    delay(1);
 }
