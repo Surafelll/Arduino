d291f93b121cb907816c363a1aab54efbb979752
Surafelll <surafelwondimagegn01@gmail.com>
2022-06-06 09:20:30 +0300

    commented

--- blink.ino
+++ blink.ino
@@ -9,6 +9,6 @@ void loop()
 {
     digitalWrite(2, HIGH); 
     delay(1000);           
-    digitalWrite(2, LOW); 
-    delay(1000);           
+    digitalWrite(2, LOW);  // turn the LED off by making the voltage LOW
+    delay(1000);           // wait for a second
 }
