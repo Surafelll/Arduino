825dcc851719cd1ea9cece1854835a15ebe8d2f9
Surafelll <surafelwondimagegn01@gmail.com>
2022-06-06 09:23:48 +0300

    commented

--- helloworld.ino
+++ helloworld.ino
@@ -1,7 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); //serial port
-  Serial.println("Hello World!");
+  Serial.println("Hello World!"); //print this
 }
 
 void loop() {
