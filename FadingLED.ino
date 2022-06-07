4fc8db28059d9d654f551d6e02953ebf96ed6338
Surafelll <surafelwondimagegn01@gmail.com>
2022-06-07 09:32:33 +0300

    commnted

--- FadingLED.ino
+++ FadingLED.ino
@@ -17,11 +17,11 @@ void loop()
     analogWrite(led, brightness);
     
     brightness = brightness + fadeAmount;
-   
+    // reverse the direction of the fading at the ends of the fade:
     if (brightness == 0 || brightness == 255)
     {
         fadeAmount = -fadeAmount;
     }
-   
+    // wait for 30 milliseconds to see the dimming effect
     delay(300);
 }
